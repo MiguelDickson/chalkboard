2f73da851817a63cb5935c4b57d0376b9f23ae31
Clint Liddick <clint.liddick@gmail.com>
2014-09-16 18:09:34 -0400

    controler and app.yaml to serve intro page via templates and static folders

--- app/Controler.py
+++ app/Controler.py
@@ -6,6 +6,7 @@ Created on Sep 11, 2014
 
 import webapp2 
 import logging
+import string
 
 # logging setup
 # TODO set to INFO in production
@@ -15,18 +16,58 @@ logging.getLogger(__name__).setLevel(logging.DEBUG)
 class IntroHandler(webapp2.RequestHandler):
     """RequestHandler for initial intro page"""
     
-    tempHtml = '<html><head><title>test</title></head><body><h2>Hello!</h2></body></html>'
-    # TODO parse template files
-    
     def get(self):
-        """GET request handler"""
+        """Intro page GET request handler"""
         logging.debug('GET request: ' + str(self.request))
+
+        # create raw html string for template
+        htmlstr = self.buildIntroHTMLString()
+        
+        # create template
+        htmltmpl = string.Template(htmlstr)
+        
+        # map dynamic content elements
+        mapping = self.buildIntroMapping()
+        
+        # substitute mapped tags
+        htmlout = htmltmpl.substitute(mapping)
+        
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(self.tempHtml)
+        self.response.write(htmlout)
+        
+        
+    def buildIntroHTMLString(self):
+        htmlstr = ''
+        with open('html/head.tmpl', 'r') as headfile:
+            htmlstr += headfile.read()
+        
+        with open('html/header.tmpl', 'r') as headerfile:
+            htmlstr += headerfile.read()
+            
+        with open('html/body.tmpl', 'r') as bodyfile:
+            htmlstr += bodyfile.read()
+            
+        with open('html/footer.tmpl', 'r') as footerfile:
+            htmlstr += footerfile.read()
+            
+        return htmlstr
+    
+    
+    def buildIntroMapping(self):
+        """ Determine and return template tag mapping """
+        mapping = dict()
+        mapping['page_title'] = 'Pitt Chalkboard'
+        return mapping
+    
+    
+    def handle_exception(self, exception, debug):
+        # overrides the built-in master exception hanlder
+        logging.error('Template mapping exception, unmapped tag: ' + str(exception))
+        return self.redirect(uri='/error', code=307) # TODO create general error page
 
 
-
-# list of URI/Handler tuples
+# list of URI/Handler routing tuples
+# the URI is a regular expression beginning with root '/' char
 routeHandlers=[
         (r'/',IntroHandler),
 ]
