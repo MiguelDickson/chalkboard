b430db4e362c10723867c794849867266bf45068
Mario Minyon <mariominyon@gmail.com>
2014-10-14 22:31:09 -0400

    Added instructor login capabilities.

--- app/Controler.py
+++ app/Controler.py
@@ -12,6 +12,12 @@ logging.getLogger(__name__).setLevel(logging.DEBUG)
 
 
 # General Utilities
+class UserData(db.Model) :
+	user_id = db.StringProperty()
+	course_name = db.StringProperty()
+	user_name = db.StringProperty()
+	user_email = db.StringProperty()
+
 def renderTemplate(response, templatename, templatevalues) :
     basepath = os.path.split(os.path.dirname(__file__)) #extract the base path, since we are in the "app" folder instead of the root folder
     path = os.path.join(basepath[0], 'templates/' + templatename)
@@ -54,25 +60,109 @@ class IntroHandler(webapp2.RequestHandler):
         return self.redirect(uri='/error', code=307)
 
 class InstructorHandler(webapp2.RequestHandler):
-    """RequestHandler for instructor page"""
+	"""RequestHandler for instructor page"""
+	def post(self):
+		name = self.request.get('name')
+		email = self.request.get('email')
+		course = self.request.get('course')
+		
+		logging.debug('InstructorHandler POST request: ' + str(self.request))
+	
+		user = users.get_current_user();
+		
+		template_values = {}
+		if user:
+			#stores data
+			user_data = UserData()
+			
+			user_data.user_id = user.user_id()
+			user_data.user_name = name			
+			user_data.user_email = email			
+			user_data.course_name = course
+			
+			user_data.put()
+			
+			logout_url = users.create_logout_url('/')
+			
+			template_values = {
+				'page_title' : "Chalkboard",
+				'current_year' : date.today().year,
+				'logout' : logout_url,
+				'email' : user_data.user_email,
+				'nickname' : user_data.user_name,
+				'course' : user_data.course_name,
+			}
+			
+		else:
+			self.redirect(users.create_login_url('/instructor'))
 
-    def get(self):
-        """Instructor page GET request handler"""
-        logging.debug('InstructorHandler GET request: ' + str(self.request))
+		#redirects back to instructor page (should show new data)
+		renderTemplate(self.response, 'instructor.html', template_values)
+	
+	def get(self):
+		"""Instructor page GET request handler"""
+		logging.debug('InstructorHandler GET request: ' + str(self.request))
+	
+		#retrieve the current user
+		user = users.get_current_user()
 		
-        template_values = {
-            'page_title' : "Chalkboard",
-            'current_year' : date.today().year
-        }
+		login_url = ''
+		logout_url = ''
 		
-        renderTemplate(self.response, 'instructor.html', template_values)
+		email = ''
+		name = ''
+		course = ''
 		
-    def handle_exception(self, exception, debug):
+		target_page = 'instructor.html'
+		
+		template_values = {
+		
+		}
+		
+		#check if signed in
+		if user:
+			logout_url = users.create_logout_url('/')
+						
+			d = UserData.all()
+			d.filter('user_id =', user.user_id())
+			
+			#if data was received, grab it
+			if d.count(1):
+				for user_data in d.run():
+					email = user_data.user_email
+					name = user_data.user_name
+					course = user_data.course_name
+				
+				template_values = {
+					'page_title' : "Chalkboard",
+					'current_year' : date.today().year,
+					'logout' : logout_url,
+					'email' : email,
+					'nickname' : name,
+					'course' : course,
+				}
+			#if no data was received, redirect to new course page (to make data)
+			else:
+				target_page = 'new_course.html'
+				template_values = {
+					'page_title' : "Chalkboard",
+					'current_year' : date.today().year,
+					'logout' : logout_url,
+					'nickname' : user.nickname(),
+				}
+				
+		else :
+			self.redirect(users.create_login_url('/instructor'))		
+		
+		
+		renderTemplate(self.response, target_page, template_values)
+
+	def handle_exception(self, exception, debug):
         # overrides the built-in master exception handler
-        logging.error('Template mapping exception, unmapped tag: ' + str(exception))
+		logging.error('Template mapping exception, unmapped tag: ' + str(exception))
         
-        return self.redirect(uri='/error', code=307)
-
+		return self.redirect(uri='/error', code=307)
+			
 class ErrorHandler(webapp2.RequestHandler):
     """Request handler for error pages"""
 
@@ -99,14 +189,13 @@ class AboutHandler(webapp2.RequestHandler) :
 
         renderTemplate(self.response, 'about.html', template_values)
 
-
 # list of URI/Handler routing tuples
 # the URI is a regular expression beginning with root '/' char
 routeHandlers = [
     (r'/', IntroHandler),
 	(r'/about', AboutHandler),
     (r'/error', ErrorHandler),
-    (r'/instructor', InstructorHandler)
+	(r'/instructor', InstructorHandler)
 ]
 
 # application object
