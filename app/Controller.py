e6c52197eab7725ba0c0da5e4970980028999736
Miguel Dickson <luis.miguel.dickson@gmail.com>
2014-10-29 23:08:40 -0400

    Current things I'm working on
    Bug fixes, sending email, new_course modifications, several changes to
    Controller

--- app/Controller.py
+++ app/Controller.py
@@ -2,7 +2,8 @@ import os
 import webapp2
 import logging
 from google.appengine.ext.webapp import template
-from google.appengine.api import users             
+from google.appengine.api import users         
+from google.appengine.api import mail    
 from google.appengine.ext import db 
 from google.appengine.ext import blobstore     
 from google.appengine.ext.webapp import blobstore_handlers         
@@ -19,7 +20,7 @@ class CourseData(db.Model):
     course_name = db.StringProperty()
     course_number = db.IntegerProperty()
     student_list = db.StringListProperty() #Stores a list of string (emails)
-    URL = db.StringProperty() #URL in the form /course/ID
+    course_id = db.StringProperty() #unique course ID
     department = db.StringProperty()
     university = db.StringProperty()
     instructor = db.StringProperty()
@@ -36,9 +37,27 @@ class UserData(db.Model) :
     user_email = db.StringProperty()
     courses = db.ListProperty(db.Key) #Stores a list of keys for courses
     is_active = db.BooleanProperty()
-	
+    current_course_selected = db.StringProperty()
+
+
 def generateURL() :
-	return "/course/123" #TODO: Generate real IDs
+    return "/course/123" #TODO: Generate real IDs
+    
+def generateClassEmails(student_list) : 
+    class_list = ""
+    l = len(student_list)
+    logging.error("The length of the student list is:")
+    logging.error(l)
+    for num in range(0, l-2):
+        class_list += student_list[num]
+        class_list += ","
+    class_list+=student_list[len(student_list)-1]
+    return class_list
+        
+
+def generateID() :
+    return '1234' #TODO: Generate real IDs
+
 
 def renderTemplate(response, templatename, templatevalues) :
     basepath = os.path.split(os.path.dirname(__file__)) #extract the base path, since we are in the "app" folder instead of the root folder
@@ -81,6 +100,20 @@ class IntroHandler(webapp2.RequestHandler):
         
         return self.redirect(uri='/error', code=307)
         
+class NewCourseHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            logout_url = users.create_logout_url('/')
+            template_values = {
+                'page_title' : "Chalkboard",
+                'current_year' : date.today().year,
+                'logout' : logout_url,
+            
+            }
+        
+        renderTemplate(self.response, 'new_course.html', template_values)
+        
 class InstructorHandler(webapp2.RequestHandler):
     """RequestHandler for instructor page"""   
     def post(self):
@@ -110,9 +143,9 @@ class InstructorHandler(webapp2.RequestHandler):
             course.department = self.request.get('department')
             course.semester = self.request.get('semester')
             course.year = int(self.request.get('year'))
-            course.student_list = [""]
+            course.student_list = ['mlucient@gmail.com']
             course.is_active = True
-            course.URL = generateURL()
+            course.course_id = generateID()
             course.documents_list = [""]
             course.syllabus = None
             
@@ -130,10 +163,12 @@ class InstructorHandler(webapp2.RequestHandler):
                 user_data.user_email = user.email()    
                 user_data.is_active = True
                 user_data.courses = [course]
+                user_data.current_course_selected = course.course_id
                 
                 user_data.put()
             else :
                 for user_data in d.run():
+                    user_data.current_course_selected = course.course_id
                     user_data.courses.append(course.key()) #Add course key to user data
                     user_data.put()
             
@@ -161,9 +196,7 @@ class InstructorHandler(webapp2.RequestHandler):
         email = ''
         name = ''
         course = ''
-        
-        target_page = 'instructor.html'
-        
+                
         template_values = {
         
         }
@@ -184,10 +217,9 @@ class InstructorHandler(webapp2.RequestHandler):
                     template_values = {
                         'page_title' : "Chalkboard",
                         'current_year' : date.today().year,
-                        'logout' : logout_url
+                        'logout' : logout_url,
+                        'courses' : CourseData.get(user_data.courses)
                     }
-                        
-                    target_page = "new_course.html"
             #if no data was received, add data entry
             else:
                 #stores data
@@ -214,7 +246,7 @@ class InstructorHandler(webapp2.RequestHandler):
             self.redirect(users.create_login_url('/instructor'))        
         
         
-        renderTemplate(self.response, target_page, template_values)
+        renderTemplate(self.response, 'instructor.html', template_values)
 
     def handle_exception(self, exception, debug):
         # overrides the built-in master exception handler
@@ -230,7 +262,7 @@ class DocumentsHandler(webapp2.RequestHandler):
         email = self.request.get('email')
         course = self.request.get('course')
         
-        logging.debug('UploadHandler POST request: ' + str(self.request))
+        logging.debug('DocumentsHandler POST request: ' + str(self.request))
     
         user = users.get_current_user();
         
@@ -300,9 +332,66 @@ class DocumentsHandler(webapp2.RequestHandler):
         logging.error('Template mapping exception, unmapped tag: ' + str(exception))
         
         return self.redirect(uri='/error', code=307)
+
+class SendEmailHandler(webapp2.RequestHandler):
+    def get(self):
+        logging.error('Here succesfully, I guess...')
+        user = users.get_current_user()
+        if user is None:
+            logging.error('SendEmail Handler: not logged in for some reason.')    
+        if user:
+            logging.error('Here all right so far.')
+            logout_url = users.create_logout_url('/')
+            d = UserData.all()
+            d.filter('user_id =', user.user_id())
+            if d.count(1):
+                logging.error('Here all right so far. #2')
+                for user_data in d.run():
+                    current_course = user_data.current_course_selected
+                    logging.error(current_course)
+                    e = CourseData.all()
+                    e.filter('course_id =', current_course)
+                    if e.count(1):
+                        logging.error('Here all right so far. #3')
+                        for course_info in e.run():
+                            students = course_info.student_list     
+                            template_values = {
+                                'current_course' : course_info,
+                                'student_list' : students,
+                                'page_title' : "Chalkboard",
+                                'current_year' : date.today().year,
+                                'logout' : logout_url            
+                            }
+                        renderTemplate(self.response, 'send_email.html', template_values)                            
+                    else:
+                        self.redirect(users.create_login_url('/instructor'))
+            else:
+                self.redirect(users.create_login_url('/insructor'))           
+        else:
+            self.redirect(users.create_login_url('/instructor'))
+        
+        
+ 
+class EmailHandler(webapp2.RequestHandler):
+    def post(self):
+        user = users.get_current_user()
+        #if user is None:
+        #    self.redirect('/instructor')
+        #    return
+        message = mail.EmailMessage()
+        message.sender = user.email()
+        stu_list = self.request.get('student_list')
+        logging.error("The list sent to the handler was: " + stu_list)
+        logging.error("The message body was:" + self.request.get('message_body'))
+        message.bcc = generateClassEmails(self.request.get('student_list'))
+        message.body = self.request.get('message_body')
+        message.to = user.email()
+        message.send()
+        
         
 class UploadHandler(blobstore_handlers.BlobstoreUploadHandler) :
     def post(self):
+        logging.debug('UploadHandler POST request: ' + str(self.request))
         upload_files = self.get_uploads('file')
         blob_info = upload_files[0];
         self.redirect(users.create_login_url('/instructor'))
@@ -333,7 +422,6 @@ class AboutHandler(webapp2.RequestHandler) :
 
     def get(self):
         logging.debug('AboutHandler GET request: ' + str(self.request))
-
         template_values = {
             'page_title' : "About Chalkboard",
             'current_year' : date.today().year
@@ -343,15 +431,46 @@ class AboutHandler(webapp2.RequestHandler) :
         
 class CourseHandler(webapp2.RequestHandler) :
     """Request handler for Course pages (public view)"""
-    def get(self):
-        logging.debug('CourseHandler GET request: ' + str(self.request))
+    def get(self, id):
+        logging.debug('CourseHandler GET request: ' + str(self.request) + id)
         
-        template_values = {
-            'page_title' : "About Chalkboard",
-            'current_year' : date.today().year
-        }
+        user = users.get_current_user()
         
-        renderTemplate(self.response, 'error.html', template_values) #TODO: temporary redirect
+        login_url = ''
+        logout_url = ''
+        
+        if(user):
+            logout_url = users.create_logout_url('/')
+        else:
+            login_url = users.create_login_url('/')
+            
+        d = CourseData.all()
+        d.filter('course_id =', id)
+            
+        if d.count(1):
+            for course in d.run():
+                template_values = {
+                    'page_title' : "Chalkboard",
+                    'current_year' : date.today().year,
+                    'logout' : logout_url,
+                    'login' : login_url,
+                    'course_name' : course.course_name,
+                    'course_number' : course.course_number,
+                    'student_list' : course.student_list,
+                    'department' : course.department,
+                    'university' : course.university,
+                    'instructor' : course.instructor,
+                    'email' : course.email,
+                    'year' : course.year,
+                    'semester' : course.semester,
+                    'is_active' : course.is_active,
+                    'course_id' : course.course_id
+                }
+            print "JHIDS"    
+            renderTemplate(self.response, 'course.html', template_values) 
+        else:
+        #redirect to error if course wasn't found (or if 2 courses share an ID???)
+            renderTemplate(self.response, 'error.html', template_values)
 
 # list of URI/Handler routing tuples
 # the URI is a regular expression beginning with root '/' char
@@ -360,9 +479,13 @@ routeHandlers = [
     (r'/about', AboutHandler),
     (r'/error', ErrorHandler),
     (r'/instructor', InstructorHandler),
+    (r'/new_course', NewCourseHandler),
     (r'/documents', DocumentsHandler),
     (r'/upload', UploadHandler),
+    (r'/send_email', SendEmailHandler),
+    (r'/email', EmailHandler),
     (r'/course/.*', CourseHandler), #Default catch all to handle a course page request
+    (r'/course/(\d+)', CourseHandler), #Default catch all to handle a course page request
     (r'/.*', ErrorHandler)
 ]
 
